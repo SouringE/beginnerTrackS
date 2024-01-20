1c9f8bd8ac2bf121bf905b78cafecbdfae21e608
SouringE <64752955+SouringE@users.noreply.github.com>
2024-01-19 21:47:21 -0600

    adjusted data to not include Unknown sex

--- parsing.py
+++ parsing.py
@@ -5,11 +5,13 @@ fda_subUniversal = fda_data.drop(columns = ['AGE_UNITS', 'CASE_OUTCOME'])
 
 fda_subUniversal['PRODUCT'] = fda_subUniversal['PRODUCT'].str.lower()
 med_case = fda_subUniversal.dropna(subset=['PRODUCT'])
+med_case = med_case.dropna(axis = 1, how = "all")
 straw = med_case[med_case['PRODUCT'].str.contains('strawb', na = False)]
-print(straw)
+
 word_freq = med_case.PRODUCT.str.split(expand = True).stack().value_counts()
 word_freq.to_csv('wordFreq.csv')
 #straw = med_case['PRODUCT'].str.contains('strawb')
-print(med_case.columns)
+women_data = med_case[med_case['SEX'].str.startswith("Male") == False]
+women_data = med_case[med_case['SEX'].str.startswith("Unknown") == False]
+women_data.to_csv('women_data.csv')
 
-print(fda_data.shape)
